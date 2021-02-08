a786a29fee6e24d1a127d0c13d734740fe634121
minseo <kim-west00@naver.com>
2021-02-08 23:55:04 +0900

    화면 기본설정

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init() #초기화
+
+#화면크기 설정
+screen_width = 480  #가로(너비)
+screen_height = 640 #세로
+pygame.display.set_mode((screen_width,screen_height))
+
+#화면 타이틀 설정
+pygame.display.set_caption("Game_Minseo")
