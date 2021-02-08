7a4507fe8a8a9d0e262ca0ea543770b5d91e2a4b
minseo <kim-west00@naver.com>
2021-02-09 01:22:37 +0900

    displat.update()추가_배경색상 변경 적용

--- pygame_basic/Oracsil.py
+++ pygame_basic/Oracsil.py
@@ -18,6 +18,7 @@ while running:
         if event.type == pygame.QUIT: #창을 닫을때(x표시)
             running = False #그때 창을 종료하자
     screen.blit(background,(0,0))#배경그리기_(0,0)은 background 나타나는 위치  
-         
+    #참고#screen.fill((0,0,255))#rgb이용해서 배경색 채우기
+    pygame.display.update()#게임화면 다시그리기_이걸로 계속 업데이트됨   
 #pygame 종료
 pygame.quit()
