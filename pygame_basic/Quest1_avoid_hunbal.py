e15af1efc0fee79287676bbb1b524a6857c491f4
minseo <kim-west00@naver.com>
2021-02-13 22:43:55 +0900

    적 x좌표 랜덤설정+(!)적 화면상 재출력 기능 구현 시도

--- pygame_basic/Quest1_avoid_hunbal.py
+++ pygame_basic/Quest1_avoid_hunbal.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 pygame.init()
+
 
 #화면크기 설정
 screen_width = 480  #가로(너비)
@@ -17,7 +19,7 @@ character_width = character_size[0]#캐릭터 가로크기
 character_height = character_size[1]#캐릭터 가로크기
 #캐릭터는 기본위치
 character_x_pos = (screen_width / 2) - (character_width/2) #화면 가로의 절반크기에 해당하는 곳에 위치하도록(가로)
-character_y_pos = screen_height - character_height  #화면 하단부에 위치하도록(세로)
+character_y_pos = screen_height - character_height #화면 하단부에 위치하도록(세로)
 #이동할 좌표
 to_x = 0
 to_y = 0
@@ -29,13 +31,13 @@ enemy_width = enemy_size[0]#캐릭터 가로크기
 enemy_height = enemy_size[1]#캐릭터 가로크기
 
 #캐릭터는 기본위치
-enemy_x_pos = (screen_width / 2) - (enemy_width/2) #화면 가로의 절반크기에 해당하는 곳에 위치하도록(가로)
-enemy_y_pos = screen_height- enemy_height/2 #화면 하단부에 위치하도록(세로)
+enemy_x_pos = random.randint(0, screen_width) #화면 가로의 절반크기에 해당하는 곳에 위치하도록(가로)
+enemy_y_pos = (screen_height/2)- (enemy_height/2) #화면 하단부에 위치하도록(세로)
 
 #FPS(화면속도 설정_Frame Per Seconds)
 clock = pygame.time.Clock()
 character_speed = 0.6#속도 설정
-
+enemy_speed = 0.4#적 투하속도 조정
 
 start_ticks = pygame.time.get_ticks()#시작 시간 정보(tick)을 받아옴
 
@@ -89,7 +91,7 @@ while running:
 
     character_x_pos += to_x * dt
     character_y_pos += to_y * dt
-    enemy_y_pos += dt * character_speed
+    enemy_y_pos += dt * enemy_speed
     #3.이벤트 처리(키보드, 마우스에 따른)
     '''pygame.event.get()#발생한 이벤트
         pygame.QUIT , KEYDOWN ,K_LEFT'''
@@ -98,6 +100,11 @@ while running:
     screen.blit(background, (0,0))
     screen.blit(character,(character_x_pos,character_y_pos))
     screen.blit(enemy,(enemy_x_pos,enemy_y_pos))
+    if enemy_y_pos < 0:
+        enemy_x_pos = random.randint(0, screen_width) #화면 가로의 절반크기에 해당하는 곳에 위치하도록(가로)
+        enemy_y_pos = (screen_height/2)- (enemy_height/2) #화면 하단부에 위치하도록(세로)
+        screen.blit(enemy,(enemy_x_pos,enemy_y_pos))
+
     pygame.display.update()#게임화면 다시그리기_이걸로 계속 업데이트됨  
 
 
