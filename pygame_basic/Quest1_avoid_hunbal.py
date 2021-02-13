62243928cbe9b029a57919b54a1efc898141fb7e
minseo <kim-west00@naver.com>
2021-02-13 22:15:39 +0900

    character 움직임 구현

--- pygame_basic/Quest1_avoid_hunbal.py
+++ pygame_basic/Quest1_avoid_hunbal.py
@@ -43,6 +43,23 @@ while running:
 ##########################################################
     dt = clock.tick(60)#fps 설정
     
+    for event in pygame.event.get():# 어떠한 이벤트가 발생하였는가?
+        if event.type == pygame.QUIT: #창을 닫을때(x표시)
+            running = False #그때 창을 종료하자
+        if event.type == pygame.KEYDOWN:#키가 눌러졌냐?
+            if event.key == pygame.K_LEFT:#캐릭터 왼쪽으로
+                to_x -= character_speed
+            elif event.key ==pygame.K_RIGHT:#캐릭터 오른쪽으로
+                to_x +=character_speed
+            elif event.key == pygame.K_UP:#캐릭터 위쪽으로
+                to_y -= character_speed
+            elif event.key == pygame.K_DOWN:#캐릭터 아래쪽으로
+                to_y += character_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x=0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y=0        
     character_x_pos += to_x * dt
     character_y_pos += to_y * dt
 
@@ -51,9 +68,9 @@ while running:
         pygame.QUIT , KEYDOWN ,K_LEFT'''
     #4. 게임 캐릭터 위치 정의
     #5. 충돌 처리 rect.colliderect()
-    
-    #6. 화면에 그리기 blit(출력대상,좌표)
-
+    screen.blit(background, (0,0))
+    screen.blit(character,(character_x_pos,character_y_pos))
+    screen.blit(enemy,(enemy_x_pos,enemy_y_pos))
     pygame.display.update()#게임화면 다시그리기_이걸로 계속 업데이트됨  
 
 pygame.init()
