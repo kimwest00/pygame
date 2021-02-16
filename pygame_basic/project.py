565412cf6ee258ddb9ddfbb280419dd2df7c8f94
minseo <kim-west00@naver.com>
2021-02-16 23:38:26 +0900

    기본 세팅

--- pygame_basic/project.py
+++ pygame_basic/project.py
@@ -1,6 +1,21 @@
 import pygame
 pygame.init()
-#화면 설정
-#캐릭터변수설정
-#while문<-게임랜더링
+
+screen_width = 480  #가로
+screen_height = 640 #세로
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+
+pygame.display.set_caption("군침이 폭발해요")
+
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #창을 닫을때(x표시)
+            running = False
+
+    pygame.display.update()
+
 pygame.quit()
