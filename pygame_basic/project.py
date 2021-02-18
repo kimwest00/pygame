7fd2d0825f079a4b5b3172d551f59aabc50e9d37
minseo <kim-west00@naver.com>
2021-02-18 23:46:47 +0900

    배경출력

--- pygame_basic/project.py
+++ pygame_basic/project.py
@@ -4,7 +4,7 @@ pygame.init()
 screen_width = 480  #가로
 screen_height = 640 #세로
 screen = pygame.display.set_mode((screen_width,screen_height))
-
+background = pygame.image.load("C://Users//김민서//OneDrive - 한국외국어대학교//바탕 화면//게임//Pygame//gunchim_background.png")
 
 pygame.display.set_caption("군침이 폭발해요")
 
@@ -15,7 +15,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #창을 닫을때(x표시)
             running = False
-
+    screen.blit(background,(0,0))
     pygame.display.update()
 
 pygame.quit()
