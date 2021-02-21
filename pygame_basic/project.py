bc9dff53bf5815330fd6060edc3c7d79deac11ab
minseo <kim-west00@naver.com>
2021-02-21 23:54:36 +0900

    키보드조작_작동안됌

--- pygame_basic/project.py
+++ pygame_basic/project.py
@@ -14,13 +14,23 @@ character_x_pos = screen_width/2  - character_width/2
 character_y_pos = screen_height-character_height
 pygame.display.set_caption("군침이 폭발해요")
 
-
+to_x = 0
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #창을 닫을때(x표시)
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_LEFT:
+                to_x -= 15
+            elif event.type == pygame.K_RIGHT:
+                to_x += 15
+
+        if event.type == pygame.KEYUP:
+            to_x = 0
+    character_x_pos += to_x
+
     screen.blit(background,(0,0))
     screen.blit(character,(character_x_pos,character_y_pos))
     pygame.display.update()
