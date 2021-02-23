0dbda72a21a3d6ca81f67764b7883858e7376f6a
minseo <kim-west00@naver.com>
2021-02-23 22:12:33 +0900

    enemy 기본설정

--- pygame_basic/project.py
+++ pygame_basic/project.py
@@ -12,6 +12,15 @@ character_width = character_size[0]
 character_height = character_size[1]
 character_x_pos = screen_width/2  - character_width/2
 character_y_pos = screen_height-character_height
+
+enemy = pygame.image.load("C://Users//김민서//OneDrive - 한국외국어대학교//바탕 화면//게임//Pygame//hunbal_enemy.png")
+enemy = pygame.transform.scale(enemy, (50, 50))
+enemy_size =  enemy.get_rect().size#이미지 크기를 가져옴
+enemy_width = enemy_size[0]#캐릭터 가로크기
+enemy_height = enemy_size[1]#캐릭터 가로크기
+enemy_x_pos = (screen_width / 2) - (enemy_width/2) #화면 가로의 절반크기에 해당하는 곳에 위치하도록(가로)
+enemy_y_pos = (screen_height/2) - (enemy_height/2)  #화면 하단부에 위치하도록(세로)
+
 pygame.display.set_caption("군침이 폭발해요")
 clock = pygame.time.Clock()
 
@@ -41,6 +50,7 @@ while running:
 
     screen.blit(background,(0,0))
     screen.blit(character,(character_x_pos,character_y_pos))
+    screen.blit(enemy,(enemy_x_pos,enemy_y_pos))
     pygame.display.update()
 
 pygame.quit()
