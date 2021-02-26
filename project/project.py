b30c72f378e6335d069bbf6b5458dec78b15ffa3
minseo <kim-west00@naver.com>
2021-02-26 23:18:58 +0900

    path이용해서 사진 불러오기

--- project/project.py
+++ project/project.py
@@ -1,5 +1,5 @@
 import pygame
-
+import os
 pygame.init() #초기화
 
 
@@ -12,13 +12,20 @@ pygame.display.set_caption("게임이름")
 #FPS(화면속도 설정_Frame Per Seconds)
 clock = pygame.time.Clock()
 
+current_path = os.path.dirname(__file__)
+image_path = os.path.join(current_path,"images")
+
+background = pygame.image.load(os.path.join(image_path,"gunchim_background.png"))
+
 running = True #게임 진행중?
 while running:
 
     dt = clock.tick(60)#fps 설정
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #창을 닫을때(x표시)
+            running = False
     
-
+    screen.blit(background,(0,0))
     pygame.display.update()
 
 #pygame 종료
